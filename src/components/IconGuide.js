6699f2d1af2c07ed7b4f53c56a561ec71fad37dc
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-17 12:11:07 -0700

    Changed classes to stateless components added Icon.js to further refactor code

--- src/components/IconGuide.js
+++ src/components/IconGuide.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import IconReferenceList from './IconReference'
-class IconGuide extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const IconGuide = (props) => {
+  return(
+    <div>
+      <h2>Launch Academy Guide</h2>
+      <p>Here are all the symbols you need to know to understand our curriculum!</p>
+      <IconReferenceList />
+    </div>
+  )  
+}
 
-  render() {
-    return(
-      <div>
-        <h2>Launch Academy Guide</h2>
-        <p>Here are all the symbols you need to know to understand our curriculum!</p>
-        <IconReferenceList />
-      </div>
-    )
-  }
-};
+
 
 export default IconGuide;
